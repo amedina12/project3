6b1f4caf9fdae5d4f07feeadf6c6bc93a11978e9
amedina12 <amedina12@hawk.iit.edu>
2023-11-15 02:38:04 -0600

    adding map

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-function init(){
-
-
-
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
 }
-window.addEventListener('load', init);
