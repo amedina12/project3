f9924f6ba26936b384838dc7f01f7d05c1052b52
amedina12 <amedina12@hawk.iit.edu>
2023-11-16 04:02:18 -0600

    added map, text, queries

--- js/script.js
+++ js/script.js
@@ -25,3 +25,56 @@ $(document).ready(function(){
         }
     });
 });
+
+function initMap() {
+
+    var iceland = { lat: 64.9631, lng: -19.0208 };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7, 
+      center: iceland,
+    });
+
+    var marker = new google.maps.Marker({
+      position: iceland,
+      map: map,
+      title: 'Iceland',
+      icon: 'images/mapmarker.png'
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: 'This is Iceland, enjoy the views!'
+      });
+      
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+
+    var trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+
+    var reykjavikCoords = [
+        { lat: 64.1265, lng: -21.8174 }, 
+        { lat: 64.2035, lng: -21.7806 },
+        { lat: 64.2035, lng: -21.6934 },
+        { lat: 64.1367, lng: -21.6046 },
+        { lat: 64.0680, lng: -21.7268 },
+        { lat: 64.0532, lng: -21.8195 },
+        { lat: 64.0680, lng: -21.9123 },
+        { lat: 64.1367, lng: -22.0345 },
+        { lat: 64.2035, lng: -21.9457 } 
+      ];
+      
+      var reykjavikPolygon = new google.maps.Polygon({
+        paths: reykjavikCoords,
+        strokeColor: '#FFA500',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FFA500',
+        fillOpacity: 0.35
+      });
+      
+      reykjavikPolygon.setMap(map);
+      
+      
+  }
