b74f2abdce5e493b3a6eb474abef607123d19347
amedina12 <amedina12@hawk.iit.edu>
2023-11-16 01:04:48 -0600

    fixing slider

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
 $(document).ready(function(){
     $('.next').on('click', function(){
-       var currentImg = $('.active');
-       var nextImage = currentImg.next();
- 
-       if(nextImage.length){
-           currentImg.removeClass('active').css('z-index', -10);
-           nextImage.addClass('active').css('z-index', 10);
-       }
+        var currentImg = $('.active');
+        var nextImage = currentImg.next();
+
+        if(nextImage.length){
+            currentImg.removeClass('active');
+            nextImage.addClass('active');
+        } else {
+            currentImg.removeClass('active');
+            $('.slider-inner img:first-child').addClass('active');
+        }
     });
- 
-     $('.prev').on('click', function(){
-         var currentImg = $('.active');
-         var prevImage = currentImg.prev();
- 
-         if(prevImage.length){
-             currentImg.removeClass('active').css('z-index', -10);
-             prevImage.addClass('active').css('z-index', 10);
-         }
-     });
- });
+
+    $('.prev').on('click', function(){
+        var currentImg = $('.active');
+        var prevImage = currentImg.prev();
+
+        if(prevImage.length){
+            currentImg.removeClass('active');
+            prevImage.addClass('active');
+        } else {
+            currentImg.removeClass('active');
+            $('.slider-inner img:last-child').addClass('active');
+        }
+    });
+});
